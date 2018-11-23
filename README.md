ae20b5af25eb2d0109a995445be9ca9a84125fe6
Kavish <kavish.munjal@cerner.com>
2018-11-22 19:03:19 -0600

    Added ability to view mock posts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+My first Vue application. An Instagram clone. 
+
+Built using the Medium tutorial here: https://medium.com/fullstackio/tutorial-build-an-instagram-clone-with-vue-js-and-cssgram-24a9f3de0408
+
 ## Build Setup
 
 ``` bash
@@ -7,4 +11,3 @@ npm install
 # serve with hot reload at localhost:8080
 npm run dev
 ```
-
---
+++ src/components/InstagramPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="instagram-post">
+    <div class="header level">
+        <div class="level-left">
+          <figure class="image is-32x32">
+            <img :src="post.userImage" />
+          </figure>
+          <span class="username">{{post.username}}</span>
+        </div>
+    </div>
+    <div class="image-container"
+      :class="post.filter"
+      :style="{ backgroundImage: 'url(' + post.postImage + ')' }"
+    />
+    <div class="content">
+      <div class="heart">
+        <i class="far fa-heart fa-lg" />
+        <span class="likes">{{post.likes}} likes</span>
+      </div>
+      <p class="caption">
+        <span class="username">{{post.username}}</span>{{post.caption}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InstagramPost',
+  props: {
+    post: Object,
+  },
+};
+</script>
+
+<style lang="scss" src="../styles/instagram-post.scss">
+// Styles from stylesheet
+</style>
